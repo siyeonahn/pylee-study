d37473357e554dc469cf6bbd488803530e3541f4
syieonahn <syieon0464@naver.com>
2024-11-05 18:20:28 +0900

    Create 1week.py

---
+++ 1week.py
@@ -0,0 +1,174 @@
+# 혼공파 1강
+# print ("Hello Python")
+# print ("Hello!" * 3)
+
+# 혼공파 2강
+# print ("안시연\t 21살\t 서울특별시 노원구")
+# print ("아롬이는 귀여워\n아롬이는 천사야 \n아롬이는 세상에서 제일 예뻐")
+
+# Enter 키 여러 줄 문자열
+# print ("""아롬이는 귀여워
+# 아롬이는 천사야
+# 아롬이는 세상에서 제일 귀여워""")
+# 컴퓨터는 1부터 시작이 아닌 0부터 시작
+# []는 연산자, 연산자를 이용해 특정 위치의 문자를 참조하는 것은 인덱싱 [:]연산자를 이용해 문자열의 일부를 추출하는 것을 슬라이상
+# print ("귀여워"[0])
+# print ("귀여워"[1])
+# print ("귀여워"[2])
+# print ("귀여워"[2])
+# print ("귀여워"[1])
+# print ("귀여워"[0])
+# len()함수를 사용하면 문자열의 길이를 구할 수 있음(띄어쓰기도 한 글자)
+# print (len("안녕하십까 여러분"))
+# print("# 연습 문제")
+# print("\\\\\\\\")
+# print ("-"*8)
+# print("안녕하세요"[1])
+# print("안녕하세요"[2])
+# print("안녕하세요"[3])
+# print("안녕하세요"[4])
+# print("안녕하세요"[5])
+# print("안녕하세요"[1:3])
+# print("안녕하세요"[2:4])
+# print("안녕하세요"[1:])
+# print("안녕하세요"[:3])
+# print("# 기본적인 연산")
+# print(15, "+", 4, "=", 15 + 4)
+# print(15, "-", 4, "=", 15 - 4)
+# print(15, "*", 4, "=", 15 * 4)
+# print(15, "/", 4, "=", 15 / 4)
+# ""안의 텍스트는 계산하지 마세요
+# print("-몫:", 9542//19)
+# print("-나머지:", 9542%19)
+# print(2+2-2*2/2*2)
+# print(2-2+2/2*2+2)
+# 변수 선언과 할당
+# pi = 3.14159265
+# r = 10
+# # 변수 참조
+# print("원주율 =", pi)
+# print("반지름 =", r)
+# print("원의 둘레 =", 2*pi*r) #원의 둘레
+# print("원의 넓이 =", pi*r*r) #원의 넓이
+#string은 문자열 ,int 정수 ,boll 참과거짓 ,float 실수 =>자료형
+# 입력을 받습니다.
+# 52273은 문자(string)야 라고 세뇌를 시킴..나야 string... 
+# string = input("입력>")
+# 출력합니다
+# print("자료:", string)
+# print("자료형:", type(string)) type이란 함수는 string의 자료형을 확인하는 용도
+# 입력을 받습니다.
+# p120~153 직접 해보는 손코딩과 과제는 추후에..^^
+
+# 혼공파 1강
+# print ("Hello Python")
+# print ("Hello!" * 3)
+# print("IDLE에서 파이썬 코드를")
+# print("작성해서 출력해 보는")
+# print("예제입니다")
+# print ("안녕하세요", "저는","배가", "고픕니다")
+# 실행되는 모든 한 줄 코드는 문장이다
+# 예제가 아닌 지식들은 책 읽기
+
+# 혼공파 2강
+# print ("안시연\t 21살\t 서울특별시 노원구")
+# print ("아롬이는 귀여워\n아롬이는 천사야 \n아롬이는 세상에서 제일 예뻐")
+# print ('안녕하세요') # 작은 따음표로도 가능
+# print ('"안녕하세요"라고 말했습니다')
+# print ("\"안녕하세요\"라고 말했습니다") #\", \" : 각각 큰따옴표, 작은따옴표
+# print("안녕하세요\n안녕하세요") # \n : 줄바꿈, 그냥 밑에 써도 줄바꿈이 됨
+# print ("안녕하세요\t안녕하세요") # \t : 탭
+# print ("안녕" + "하세요")
+# Enter 키 여러 줄 문자열
+# print ("""아롬이는 귀여워
+# 아롬이는 천사야
+# 아롬이는 세상에서 제일 귀여워""")
+# 컴퓨터는 1부터 시작이 아닌 0부터 시작
+# []는 연산자, 연산자를 이용해 특정 위치의 문자를 참조하는 것은 인덱싱 [:]연산자를 이용해 문자열의 일부를 추출하는 것을 슬라이상
+# print ("귀여워"[0])
+# print ("귀여워"[1])
+# print ("귀여워"[2])
+# print ("귀여워"[2])
+# print ("귀여워"[1])
+# print ("귀여워"[0])
+# len()함수를 사용하면 문자열의 길이를 구할 수 있음(띄어쓰기도 한 글자)
+# print (len("안녕하십까 여러분"))
+# print("# 연습 문제")
+# print("\\\\\\\\")
+# print ("-"*8)
+# print("안녕하세요"[1])
+# print("안녕하세요"[2])
+# print("안녕하세요"[3])
+# print("안녕하세요"[4])
+# print("안녕하세요"[5])
+# print("안녕하세요"[1:3]) # 3인 세가 나오는 것이 아닌 뒤의 숫자 -1인 하 가 나와야 함
+# print("안녕하세요"[2:4])
+# print("안녕하세요"[1:])
+# print("안녕하세요"[:3])
+# print("# 기본적인 연산")
+# print(15, "+", 4, "=", 15 + 4)
+# print(15, "-", 4, "=", 15 - 4)
+# print(15, "*", 4, "=", 15 * 4)
+# print(15, "/", 4, "=", 15 / 4)
+# ""안의 텍스트는 계산하지 마세요
+# print("-몫:", 9542//19)
+# print("-나머지:", 9542%19)
+# print(2+2-2*2/2*2)
+# print(2-2+2/2*2+2)
+# 변수 선언과 할당
+# pi = 3.14159265
+# r = 10
+# # 변수 참조
+# print("원주율 =", pi)
+# print("반지름 =", r)
+# print("원의 둘레 =", 2*pi*r) #원의 둘레
+# print("원의 넓이 =", pi*r*r) #원의 넓이
+# string은 문자열 ,int 정수 ,boll 참과거짓 ,float 실수 =>자료형
+# 입력을 받습니다.
+# 52273은 문자(string)야 라고 세뇌를 시킴..나야 string... 
+# string = input("입력>")
+# 출력합니다
+# print("자료:", string)
+# print("자료형:", type(string)) type이란 함수는 string의 자료형을 확인하는 용도
+# 입력을 받습니다.
+# string = input("입력>")
+# 출력합니다.
+# print("자료:", string)
+# print("자료형:", type(string))
+# 입력을 받습니다.
+# string = input("입력>")
+# 출력합니다
+# print("입력 + 100:", string + 100)
+# string_a = input("입력A>")
+# int_a = int(string_a)
+# string_b = input("입력B> ")
+# int_b = int(string_b)
+# print("문자열 자료:", string_a + string_b)
+# print("숫자 자료:", int_a + int_b)
+# output_a = int("52")
+# output_b = float("52.273")
+# print (type(output_a), output_a)
+# print (type(output_b), output_b)
+# input_a = float(input("첫 번째 숫자>"))
+# input_b = float(input("두 번째 숫자>"))
+# print("덧셈 결과:", input_a + input_b)
+# print("뺄셈 결과:", input_a - input_b)
+# print("곱셈 결과:", input_a * input_b)
+# print("나눗셈 결과:", input_a / input_b)
+# output_a = str(52)
+# output_b = str(52.273)
+# print(type(output_a), output_a)
+# print(type(output_b), output_b)
+# 숫자를 입력받습니다.
+# raw_input = input("inch 단위의 숫자를 입력해주세요:")
+# 입력받은 데이터를 숫자 자료형으로 변경하고, cm단위로 변경합니다.
+# inch = int(raw_input)
+# cm = inch * 2.54
+# 출력합니다
+# print(inch, "inch는 cm 단위로", cm, "cm입니다.")
+# p.130 마무리 문제 풀기
+# format() 함수로 숫자를 문자열로 변환하기
+string_a = "{}".format(10) 
+# 출력하기
+print(string_a)
+print(type)
